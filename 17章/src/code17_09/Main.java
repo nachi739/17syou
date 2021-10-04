2eba4c98624d36f6e8abfaf6c567eaa5f9613030
nachi739 <namiki081025@gmail.com>
2021-10-04 19:03:35 +0900

    code17_09

---
+++ 17章/src/code17_09/Main.java
@@ -0,0 +1,23 @@
+package code17_09;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter("data.txt");
+			fw.write("hello world!2");
+		} catch (Exception e) {
+			System.out.println("何らかの例外が発生しました。");
+		} finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+				;
+			}
+		}
+	}
+
+}
